9848ae5d7e77389dcfa022107c7d344f25542b14
wh1teee <wh1teee1337lol@gmail.com>
2020-10-25 23:37:03 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let strSymbols = str.split(''),
+        bracketsSymbols = [],
+        stackArr = []
+
+    for( let i = 0; i < bracketsConfig.length; i++ ) {
+        bracketsSymbols[bracketsConfig[i][0]] = bracketsConfig[i][1]
+    }
+
+    for( let i = 0; i < strSymbols.length; i++ ) {
+        if(strSymbols[i] === stackArr[stackArr.length - 1]) {
+            stackArr.pop()
+        } else {
+            if( bracketsSymbols[strSymbols[i]] !== undefined ) {
+                stackArr.push(bracketsSymbols[strSymbols[i]])
+            } else return false
+        }
+    }
+    return stackArr.length === 0;
 }
